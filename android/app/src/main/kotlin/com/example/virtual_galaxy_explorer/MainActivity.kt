129ff79790cd47bbc41291c37385c79487ae258c
fatmakhadidja <154927228+fatmakhadidja@users.noreply.github.com>
2024-07-20 20:17:57 +0100

    First commit

---
+++ android/app/src/main/kotlin/com/example/virtual_galaxy_explorer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.virtual_galaxy_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//     runApp(MaterialApp(
+//       routes: {
+//         '/' : launch(),
+//       },
+//     ));
+// }
+
